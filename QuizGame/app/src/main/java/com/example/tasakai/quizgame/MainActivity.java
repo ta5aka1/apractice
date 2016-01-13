fa6ab5f20a5ed758e9cb643775d0cf5c834521ff
Takayasu Sakai <cba27ag@gmail.com>
2016-01-14 02:01:06 +0900

    add onClick logic

--- QuizGame/app/src/main/java/com/example/tasakai/quizgame/MainActivity.java
+++ QuizGame/app/src/main/java/com/example/tasakai/quizgame/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.tasakai.quizgame;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // ボタンがタッチされた時の処理
+    public void onClick(View v){
+        switch (v.getId()) {
+            case R.id.button_normal:
+                Toast.makeText(this, "ノーマルがタッチされた！", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button_random:
+                Toast.makeText(this, "ランダムがタッチされた！", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.button_time:
+                Toast.makeText(this, "タイムアタックがタッチされた！", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
